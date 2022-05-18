02f7e86f73b5ffa7455379a6ff671177ee33e37f
Choikyudong <slgmgm@naver.com>
2022-05-19 00:36:23 +0900

    조금 더 원시적인 방법을 먼저 사용해봄

--- src/main/java/servlet/study/ServletStudyStart.java
+++ src/main/java/servlet/study/ServletStudyStart.java
@@ -8,31 +8,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.io.PrintWriter;
 
 @WebServlet(name = "ServletStart", value = "/main")
 public class ServletStudyStart extends HttpServlet {
-    private String message;
-
-    public void init() {
-        message = "Hello World!";
-    }
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        /**
+         * 아래의 코드가 전혀 이해가 되지 않는다면 java.io.Stream 부분을 공부하면 좋다.
+         * 아래에서는 가장 기본적인 출력방식을 이용해서 출력을 해본다.
+         */
+
+        // Response를 통해서 화면을 출력하거나 값을 웹페이지쪽으로 넘겨줄 수 있다.
+        OutputStream outPut = resp.getOutputStream();
+        // 두번째 매개변수에 true값을 넘겨주면 플러쉬를 자동으로 해준다.
+        PrintStream printStream = new PrintStream(outPut, true);
+        printStream.println("안녕 서블릿아! 반갑다~!!");
 
     }
 
-    public void destroy() {
-    }
 }
